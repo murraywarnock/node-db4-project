5a5ae73d44cccbf2731e026255534559a4912f64
MurrayWarnock <murray.warnock@gmail.com>
2021-08-12 21:47:42 -0600

    endpoint working for select SQL

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -7,7 +7,7 @@
 //     s.instructions,
 //     i.ingredient_id,
 //     i.ingredient_name,
-//     i.amount
+//     si.amount
 // from recipes r
 // join steps s 
 //     on s.recipe_id = r.recipe_id
@@ -17,8 +17,27 @@
 //     on si.ingredient_id = i.ingredient_id
 // order by r.recipe_id, s.step_number
 
-function getRecipeById(recipeId) {
-    return Promise.resolve('awesome')
+const db = require('../../data/db-config')
+
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r')
+
+        .where('s.recipe_id', recipe_id)
+        .join('steps as s', 's.recipe_id', 'r.recipe_id')
+        .leftJoin('step_ingredient as si', 'si.step_id', 's.step_id')
+        .leftJoin('ingredients as i', 'si.ingredient_id', 'i.ingredient_id')
+        .select(
+            'r.recipe_id',
+            'r.recipe_title',
+            'r.created_date',
+            's.step_id',
+            's.step_number',
+            's.instructions',
+            'i.ingredient_id',
+            'i.ingredient_name',
+            'si.amount',
+            )
+    return recipeRows
 }
 
 module.exports = { getRecipeById };
